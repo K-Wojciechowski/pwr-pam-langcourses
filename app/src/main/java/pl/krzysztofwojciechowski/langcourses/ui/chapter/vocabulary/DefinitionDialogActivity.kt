6279acc6c69f2f48441db8bd060b833f9eab667b
Krzysztof Wojciechowski <242409@student.pwr.edu.pl>
2019-12-06 01:05:55 +0100

    Improve appearance of definition dialog

--- app/src/main/java/pl/krzysztofwojciechowski/langcourses/ui/chapter/vocabulary/DefinitionDialogActivity.kt
+++ app/src/main/java/pl/krzysztofwojciechowski/langcourses/ui/chapter/vocabulary/DefinitionDialogActivity.kt
@@ -1,11 +1,13 @@
 package pl.krzysztofwojciechowski.langcourses.ui.chapter.vocabulary
 
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_definition_dialog.*
 import pl.krzysztofwojciechowski.langcourses.*
+
 
 class DefinitionDialogActivity : AppCompatActivity() {
 
@@ -20,5 +22,16 @@ class DefinitionDialogActivity : AppCompatActivity() {
             intent.extras!!.getString(IE_DEF_TRANSLATION)
         )
         def_text.text = intent.extras!!.getString(IE_DEF_DEFINITION)
+        val displayMetrics: DisplayMetrics = applicationContext.resources.displayMetrics
+        val dpHeight = displayMetrics.heightPixels
+        val dpWidth = displayMetrics.widthPixels
+        var propWidth = 0.8 * dpWidth
+        var propHeight = 0.8 * dpWidth * def_image.drawable.intrinsicHeight / def_image.drawable.intrinsicWidth
+        if (propHeight > 0.8 * dpHeight) {
+            propHeight = 0.8 * dpHeight
+            propWidth = 0.8 * dpHeight * def_image.drawable.intrinsicWidth / def_image.drawable.intrinsicHeight
+        }
+        def_image.layoutParams.height = propHeight.toInt()
+        def_image.layoutParams.width = propWidth.toInt()
     }
 }
